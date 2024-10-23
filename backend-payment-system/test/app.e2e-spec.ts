9ca78ab7e2e42d8071a83c7e8daef4fa0c3bcb13
vvvjey <tuhv.adsagency@gmail.com>
2024-10-23 14:54:40 +0700

    Api user,wallet

--- backend-payment-system/test/app.e2e-spec.ts
+++ backend-payment-system/test/app.e2e-spec.ts
@@ -1,24 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+import { INestApplication, ValidationPipe } from "@nestjs/common"
+import {Test} from '@nestjs/testing'
+import { Validate } from "class-validator"
+import { AppModule } from "src/app.module"
+describe('App EndtoEnd test',()=>{
+  let app:INestApplication
+  beforeAll(async()=>{
+    const appModule = await Test.createTestingModule({
+      imports:[AppModule]
+    }).compile()
+    app = appModule.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
+    await app.init()
+  })
+  afterAll(async()=>{
+    app.close()
+  })
+  it.todo('should PASS, keke 1');
+  it.todo('should PASSS, keke 2');
+})
