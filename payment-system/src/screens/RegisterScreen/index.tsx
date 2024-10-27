01d10050f04004f2906978107839c39d8155d99f
UIT-21522130 <96097433+UIT-21522130@users.noreply.github.com>
2024-10-27 20:29:50 +0700

    front: get input data

--- payment-system/src/screens/RegisterScreen/index.tsx
+++ payment-system/src/screens/RegisterScreen/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { Component, useEffect, useState } from "react";
 import {
   Button,
   Image,
@@ -15,8 +15,6 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../../../assets/colors";
 import { fontScale, heightScale, widthScale } from "../../utils/spacing";
 import IMAGES from "../../../assets/images";
-import TextboxRegister from "../../components/TextboxRegister";
-import ButtonRegister from "../../components/ButtonRegister";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab } from "react-native-elements";
@@ -25,6 +23,23 @@ import { registerHuong } from "../../services/apiService";
 // import ve
 // useEffect ( call thử bằng cách truyển thử data vào , r xem kết quả nhận , vào database)
 const RegisterScreen = () => {
+    const [name, setName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [password, setPassword] = useState("");
+    const handleRegister = async () => {
+      try {
+          console.log("testapi")
+          const requestData = {
+            name, phoneNumber, password          
+          };
+          console.log("Request data:", requestData); 
+          const response = await registerHuong(requestData);
+          
+          console.log("Registration response:", response.data);
+      } catch (error) {
+          console.error("Registration error:", error);
+      }
+  };
   return (
     <SafeAreaView style={styles.headerPart}>
       <View style={styles.headerContainer}>
@@ -41,12 +56,76 @@ const RegisterScreen = () => {
         <View style={styles.titleLogin}>
           <Text style={styles.title}>Đăng ký</Text>
         </View>
-        <View>
+        {/* <View>
           <TextboxRegister />
+        </View> */}
+      <View>
+        <Text style={styles.text}>Họ và tên</Text>
+        <View style={styles.pwdContainer}>
+            <View>
+                <TextInput
+                    style={styles.inputPwd}
+                    placeholder="Nhập họ và tên"
+                    value={name}
+                    onChangeText={setName}
+                />
+            </View>
         </View>
-        <View>
-          <ButtonRegister />
+        <Text style={styles.text}>Số điện thoại</Text>
+        <View style={styles.phoneContainer}>
+            <View style={styles.firstChild}>
+                <View style={styles.countryCodeContainer}>
+                    <Text style={styles.countryCode}>+84</Text>
+                </View>
+            </View>
+            <View>
+                <TextInput
+                    style={styles.inputPhone}
+                    placeholder="712345678"
+                    keyboardType="phone-pad"
+                    value={phoneNumber}
+                    onChangeText={setPhoneNumber}
+                />
+            </View>
         </View>
+        <Text style={styles.text}>Mật khẩu</Text>
+        <View style={styles.pwdContainer}>
+            <View>
+                <TextInput
+                    style={styles.inputPwd}
+                    placeholder="Nhập mật khẩu"
+                    secureTextEntry={true}
+                    value={password} 
+                    onChangeText={setPassword} 
+                />
+            </View>
+            <TouchableOpacity>
+                <Image source={IMAGES.Eye} style={styles.eye}></Image>              
+            </TouchableOpacity>
+        </View>
+        <Text style={styles.text}>Xác nhận mật khẩu</Text>
+        <View style={styles.pwdContainer}>
+            <View>
+                <TextInput
+                    style={styles.inputPwd}
+                    placeholder="Nhập mật khẩu"
+                    secureTextEntry={true}
+                    value={password} 
+                    onChangeText={setPassword} 
+                />
+            </View>
+            <TouchableOpacity>
+                <Image source={IMAGES.Eye} style={styles.eye}></Image>              
+            </TouchableOpacity>
+        </View>
+        
+      </View>
+
+
+        
+        <TouchableOpacity onPress={handleRegister} style={styles.button}>
+            <Text style={styles.buttonText}>Đăng ký</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
     );
@@ -86,5 +165,78 @@ const styles = StyleSheet.create({
     fontSize: fontScale(25),
     fontWeight: "bold",
   },
+  button: {
+    backgroundColor: Colors.MainColor,
+    borderRadius: widthScale(10),
+    height: heightScale(55),
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: heightScale(19)
+  },
+  buttonText: {
+    color: Colors.White,
+    fontSize: fontScale(17),
+    fontWeight: "500"
+  },
+  text: {
+    fontSize: fontScale(18),
+    fontWeight: "500",
+    marginLeft: widthScale(5),
+    marginBottom: heightScale(-7),
+    marginTop: heightScale(20),
+
+},
+phoneContainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: Colors.MainColor, 
+    borderRadius: 10,
+    marginTop: heightScale(22),
+    alignItems: "center",
+},
+countryCodeContainer: {
+    width: widthScale(60),  
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.MainColor, 
+},
+firstChild: {
+    backgroundColor: Colors.MainColor,
+    paddingHorizontal: heightScale(20),
+    paddingVertical: heightScale(14),
+    borderTopLeftRadius: 9,
+    borderBottomLeftRadius: 9,
+},
+countryCode: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: heightScale(18),
+},
+inputPhone: {
+    fontSize: heightScale(15),
+    color: "#7B7B7B", 
+    paddingLeft: widthScale(21),
+    width: widthScale(250)
+},
+pwdContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: Colors.MainColor, 
+    borderRadius: widthScale(10),
+    marginTop: heightScale(22),
+    paddingVertical: heightScale(14)
+},
+inputPwd: {
+    color: "#333",
+    paddingLeft: widthScale(20),
+    fontSize: heightScale(15),
+},
+eye: {
+    width: widthScale(24),
+    height: heightScale(24),
+    marginRight: widthScale(20),
+},
 }
 )
--- payment-system/src/services/apiService.ts
+++ payment-system/src/services/apiService.ts
@@ -34,7 +34,7 @@ apiClient.interceptors.response.use(
 export const testApi = () => {
     return apiClient.get('/api/v1/wallet/test');
 };
-export const registerHuong = (data: { email: string, password: string }) => {
+export const registerHuong = (data: { name: string, phoneNumber: string, password: string }) => {
     return apiClient.post('/api/v1/auth/register', data);
 };
 export const loginHuong = (data: { phoneNumber: string; password: string }) => {
