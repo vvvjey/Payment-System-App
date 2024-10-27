12ff182a54abc69bde928a423acdc43153f9f5ca
vvvjey <tuhv.adsagency@gmail.com>
2024-10-27 18:10:04 +0700

    fix api register

--- payment-system/src/components/ButtonRegister/index.tsx
+++ payment-system/src/components/ButtonRegister/index.tsx
@@ -11,7 +11,7 @@ import {
 import IMAGES from "../../../assets/images";
 import { widthScale, heightScale, fontScale } from "../../utils/spacing";
 import { Colors } from "../../../assets/colors";
-import { registerHuong } from "../../services/apiService";
+import { registerHuong,testApi } from "../../services/apiService";
 
 const ButtonRegister = () => {
     const [name, setName] = useState("");
@@ -20,7 +20,15 @@ const ButtonRegister = () => {
 
     const handleRegister = async () => {
         try {
-            const response = await registerHuong({ name, phoneNumber, password });
+            console.log("testapi")
+            const requestData = {
+                email: "hihia@gmail.com",  // Kiểm tra giá trị này
+                password: "123"          // Kiểm tra giá trị này
+            };
+            console.log("Request data:", requestData); // In ra dữ liệu
+            const response = await registerHuong(requestData);
+                // const response = await testApi();
+            
             console.log("Registration response:", response.data);
         } catch (error) {
             console.error("Registration error:", error);
